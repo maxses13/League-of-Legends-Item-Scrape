cf4ceb3abe18385fb09acba217e18d72faa5e9de
maxses13 <maxamillian.dibella@gmail.com>
2023-01-11 15:31:24 -0500

    Description has been filtered to remove html tags

--- ItemScrapeFinal.py
+++ ItemScrapeFinal.py
@@ -7,9 +7,10 @@ Created on Tue Jan 10 13:15:29 2023
 
 import requests
 import pandas as pd
+import bs4
 from pandas.io.json import json_normalize
 import json
-
+import matplotlib
 
 url = "http://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/item.json"
 R = requests.get(url)
@@ -20,5 +21,8 @@ name = pd.json_normalize(name["data"].values())
  
 name = name.drop(columns=['into','image.full','image.sprite','image.group','image.x','from','image.y','image.w','image.y','image.h','inStore','maps.21','maps.22','specialRecipe','stacks','requiredAlly','colloq'])
 
+name['description'] = name['description'].str.replace(r'<[^<>]*>', '', regex=True)
+
 name = name[name['gold.purchasable'] == True]
 
+name.to_csv('LeagueItems.csv', index=False)
